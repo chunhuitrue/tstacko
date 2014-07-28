21be36f2f57d05dc0f7dc66f5b5a46c2459d62aa
LiChunhui <chunhui_true@163.com>
2014-07-29 00:33:49 +0800

    x

---
+++ server/src/conns_sup.erl
@@ -0,0 +1,18 @@
+-module(conns_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/0]).
+
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+
+init([]) ->
+    {ok, {{one_for_one, 5, 10}, []}}.
