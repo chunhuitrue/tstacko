a5ff7a3d0b4a069d48b6d849676b5973e63d4a9d
LiChunhui <chunhui_true@163.com>
2014-07-29 00:35:29 +0800

    x

--- server/src/tserver_sup.erl
+++ server/src/tserver_sup.erl
@@ -2,26 +2,32 @@
 
 -behaviour(supervisor).
 
-%% API
 -export([start_link/0]).
 
-%% Supervisor callbacks
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
-%% ===================================================================
-%% API functions
-%% ===================================================================
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-%% ===================================================================
-%% Supervisor callbacks
-%% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    AcceptorSpec = {acceptor_sup,                     % id
+                    {acceptor_sup, start_link, []},   % {Module, Function, Arguments}
+                    permanent,                        % Restart
+                    brutal_kill,                      % Shutdown
+                    supervisor,                       % Type
+                    [accepter_sup]},                  % ModuleList
+
+    ConnsSpec = {conns_sup,                     % id
+                 {conns_sup, start_link, []},   % {Module, Function, Arguments}
+                 permanent,                     % Restart
+                 brutal_kill,                   % Shutdown
+                 supervisor,                    % Type
+                 [conns_sup]},                  % ModuleList
+
+    {ok, {{one_for_one, 5, 10}, [AcceptorSpec, ConnsSpec]}}.
 
