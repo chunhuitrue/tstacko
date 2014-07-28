caef779c6989b071a3d6c95a41137e352981f026
LiChunhui <chunhui_true@163.com>
2014-07-28 21:31:08 +0800

    x

---
+++ client/src/tclient_app.erl
@@ -0,0 +1,16 @@
+-module(tclient_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    tclient_sup:start_link().
+
+stop(_State) ->
+    ok.
